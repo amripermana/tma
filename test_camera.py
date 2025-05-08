2d218806093e45e83d526fbfe7f66a9371fa8b94
Amri Abyad Permana <amri.permana27@gmail.com>
2025-05-08 19:42:06 +0700

    2

---
+++ test_camera.py
@@ -0,0 +1,12 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Display the resulting frame
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
